56b1e7adab5bf2092ec61e4c1ec3d86acba23810
RitwickJoshi <ritwickjoshi@gmail.com>
2020-09-01 17:42:23 +0530

    Basid Data Structure in Java

---
+++ Set_data_type.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Set_data_type {
+    public static void main(String[] args) {
+        Set<String> set_dt = new HashSet<String>();
+        set_dt.add("Example");
+        System.out.println(set_dt);
+    }
+}
